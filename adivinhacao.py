6f06e8d402fb2e97745160d0c657d2d4414b86f5
WILDMAX <wdealmeidaramalho@gmail.com>
2021-12-08 11:18:19 -0300

    Jogo

--- adivinhacao.py
+++ adivinhacao.py
@@ -8,6 +8,18 @@ numero_secreto =random.randrange(1,101) #random gera um número entre 0.0 1.0
 total_de_tentativas = 3
 rodada =1
 
+print("Qual nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+
+nivel = int(input("Define o nível: "))
+
+if(nivel == 1):
+    total_de_tentativas = 20
+elif(nivel==2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
+
 print("numero {}".format(numero_secreto))
 
 'while(rodada <= total_de_tentativas):'
