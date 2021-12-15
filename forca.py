77c890375a5d5bc62c018021c12fa357132b2e23
WILDMAX <wdealmeidaramalho@gmail.com>
2021-12-14 21:24:26 -0300

    Jogo

--- forca.py
+++ forca.py
@@ -6,6 +6,22 @@ def jogar():
     print("***********************************")
 
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        chute = input("Qual letra?")
+
+        index = 0
+
+        for letra in palavra_secreta:
+            if(chute == letra):
+                print("Encontrei na letra {} na posição {}".format(letra, index))
+            index = index + 1
+
+        print("jogando ...")
+
     print("Fim de jogo")
 
     if(__name__ == "__main__"):
