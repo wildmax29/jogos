8b89af05c6461766ea3345e25b754e82e90f1168
WILDMAX <wdealmeidaramalho@gmail.com>
2021-12-17 18:37:13 -0300

    Jogo

--- forca.py
+++ forca.py
@@ -6,25 +6,37 @@ def jogar():
     print("***********************************")
 
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
     letras_acertadas =["_","_","_","_","_","_"]
     enforcou = False
     acertou = False
+    erros = 0
 
     print(letras_acertadas)
 
     while(not enforcou and not acertou):
 
         chute = input("Qual letra?")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        index = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                letras_acertadas[index]=letra
-            index = index + 1
+        if(chute and palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute.upper() == letra.upper()):
+                    letras_acertadas[index]=letra
+                index = index + 1
+        else:
+            erros = erros +1
 
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
+
+        if(acertou):
+            print("Você Ganhou!!")
+
+        else:
+            print("Você perdeu!!")
 
     print("Fim de jogo")
 
